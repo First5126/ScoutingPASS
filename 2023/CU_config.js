3a4d9cebb4a90333d1bd91119d6cdd3662bf0baf
Bpett123 <34280256+Bpett123@users.noreply.github.com>
2023-03-30 19:35:34 -0500

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023ksla",
+      "defaultValue": "2023mokc",
       "required": "true",
       "disabled": "true"
     },
